d1218dd664a6f95bdf71e24d208d3eff88b4dc91
annhuh <huhta@hotmail.se>
2016-04-26 10:54:38 +0200

    Project created, initial commit

---
+++ Tidrapport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tidrapport.Startup))]
+namespace Tidrapport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
