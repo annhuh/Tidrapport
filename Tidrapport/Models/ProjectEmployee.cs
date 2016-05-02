e08686bec3677e9be1f7e62725a172a3ebcf6ad6
annhuh <huhta@hotmail.se>
2016-05-02 16:21:24 +0200

    First ProjectEmployee

---
+++ Tidrapport/Models/ProjectEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Tidrapport.Models
+{
+    public class ProjectEmployee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
+        [ForeignKey("EmployeeId"), Column(Order = 1)]
+        public virtual Employee Employee { get; set; }
+
+        [ForeignKey("ProjectId"), Column(Order = 2)]
+        public virtual Project Project { get; set; }
+    }
+}
