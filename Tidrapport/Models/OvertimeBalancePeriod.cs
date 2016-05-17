4c76338ee7bebdaf170fcd12ba6ed47994e8a731
annhuh <huhta@hotmail.se>
2016-05-17 10:24:02 +0200

    missed model added

---
+++ Tidrapport/Models/OvertimeBalancePeriod.cs
@@ -0,0 +1,34 @@
+﻿	using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Tidrapport.Models
+{
+	public class OvertimeBalancePeriod
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Giltig från")]
+        public DateTime ValidFrom { get; set; }
+
+        [Display(Name = "Giltig till")]
+        public DateTime ValidTo { get; set; }
+            
+        [Display(Name="Mertid")]
+        public decimal OvertimeBalance1 { get; set; }
+        [Display(Name = "Övertid Enkel")]
+        public decimal OvertimeBalance2 { get; set; }
+        [Display(Name = "Övertid Kval")]
+        public decimal OvertimeBalance3 { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public virtual Employee Employee { get; set; }
+    }
+}
