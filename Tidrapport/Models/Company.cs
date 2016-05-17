79d176dfc779592da8fec1fec30ad29bc18c016e
annhuh <huhta@hotmail.se>
2016-05-17 10:30:01 +0200

    missed model added

---
+++ Tidrapport/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tidrapport.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+
+        [Display(Name = "Organisations nr")]
+        public string OrgRegNo { get; set; }
+
+        [Display(Name = "Företagsnamn")]
+        public string Name { get; set; }
+    }
+}
