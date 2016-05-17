ed3fe80323c51fd1656d3262b06131932aa2aaa1
annhuh <huhta@hotmail.se>
2016-05-17 11:21:35 +0200

    commit

---
+++ Tidrapport/ViewModels/AddEmployeesToProjetc_VM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tidrapport.Models;
+
+namespace Tidrapport.ViewModels
+{
+    public class AddEmployeesToProjetc_VM
+    {
+        
+    }
+}
---
+++ Tidrapport/ViewModels/EconomicalTimeReport_VM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Tidrapport.Models;
+
+namespace Tidrapport.ViewModels
+{
+    public class EconomicalTimeReport_VM
+    {
+        [Display(Name = "År-Vecka")]
+        public string YearWeek { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Datum")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Antal timmar")]
+        public decimal NumberOfHours { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        //[ForeignKey("EmployeeId")]
+        public virtual Employee Employee { get; set; }
+
+        public int ActivityId { get; set; }
+
+        //[ForeignKey("ActivityId")]
+        public virtual Activity Activity { get; set; }
+    }
+}
